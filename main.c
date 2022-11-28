2f426195c5488966e61127d872963ec770657dc0
cobrce <cob_rce@hotmail.com>
2022-11-28 22:35:27 +0100

    Added 7 segments and bargraph display for level and errors, code unfinished

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "MAX7219_attiny.h"
 #include "font.h"
 #include "errors.h"
+#include "ShiftRegister.h"
 #include "calibrate.h"
 
 #define HB_LED PD3
@@ -33,8 +34,140 @@
 uint16_t EEMEM EE_FullHeight = FULL_WATER + SENSOR_HEIGHT;
 volatile uint16_t FullHeight = 0;
 volatile uint16_t distance; // for debug
+volatile uint16_t levelInPercent;
 
-uint16_t MeasureDistance() // in cm
+// calculate a bargraph representation of a value then shift it to fit the shift register
+uint16_t calculateBarGraph(uint16_t valueInPercent, uint8_t shift)
+{
+    if (valueInPercent > 100)
+        valueInPercent = 100;
+    uint16_t result = 0;
+
+    valueInPercent = ((valueInPercent / 10) + 1) % 11; // each led in bargraph represents 10% rounded to the next 10%, seiled at 10
+
+    while (valueInPercent--)
+        result = (result << 1) | 1;
+
+    return result << shift;
+}
+
+// this function calculates the value of 7seg display whether it's an error code or percentage
+uint8_t errors[4] = {0};
+
+enum
+{
+    SevenSegDisplayingError,
+    SevenSegDisplayingWaterLevel
+} StateOfSevenSeg = SevenSegDisplayingWaterLevel;
+
+enum
+{
+    SevenSegFirstDigit,
+    SevenSegSecondDigit,
+    SevenSegThirdDigit
+} PhaseOfSevenSeg = SevenSegFirstDigit;
+
+uint8_t SevenSegNumbers[] = {
+    0b1110111, // 0
+    0b0110000, // 1
+    0b1101011, // 2
+    0b1111001, // 3
+    0b0111100, // 4
+    0b1011101, // 5
+    0b1011111, // 6
+    0b1110000, // 7
+    0b1111111, // 8
+    0b1111101  // 9
+};
+
+const uint8_t SevenSegP = 0b1100111;
+const uint8_t SevenSegF = 0b1000111;
+
+volatile uint8_t SevenSegErrorCodeIndex = 1; // like this 2 or 3 chars can be displayed during SevenSegSecondDigit phase and leave third digit for blank char
+uint8_t sevenSegDisplayError()
+{
+    uint8_t result = 0;
+    switch (PhaseOfSevenSeg)
+    {
+    case SevenSegFirstDigit:
+        SevenSegErrorCodeIndex = 1;
+        PhaseOfSevenSeg = SevenSegSecondDigit;
+        result = SevenSegF;
+        break;
+
+    case SevenSegSecondDigit:
+    {
+        uint8_t value = errors[0];
+        for (int i = 0; i < SevenSegErrorCodeIndex; i++)
+            value /= 10;
+        value %= 10;
+        result = SevenSegNumbers[value]; // convert number into 7 seg representation
+
+        if (SevenSegErrorCodeIndex == 0) // shifting the errors queue
+        {
+            for (uint8_t i = 0; i < sizeof(errors); i++)
+                errors[i] = errors[i] + 1;
+            errors[sizeof(errors)-1] = 0;
+            PhaseOfSevenSeg = SevenSegThirdDigit;
+        }
+        else
+        {
+            SevenSegErrorCodeIndex--;
+        }
+    }
+    break;
+
+    case SevenSegThirdDigit:
+        StateOfSevenSeg = SevenSegDisplayingWaterLevel; // the last digit of this error is displayed, switching back to water level display, if another error is in queue "Calculate7Seg" will call this again
+        PhaseOfSevenSeg = SevenSegFirstDigit;
+        break;
+    }
+    return result;
+}
+
+uint8_t sevenSegDisplayWaterLevel()
+{
+    return 0;
+}
+
+volatile uint32_t lastSevenSegUpdate = 0;
+volatile uint8_t previous7SegValue = 0;
+uint8_t calculate7Seg()
+{
+    uint32_t now = millis();
+    if (lastSevenSegUpdate && (now - lastSevenSegUpdate) < 500)
+        return previous7SegValue;
+
+    lastSevenSegUpdate = now;
+
+    if (StateOfSevenSeg == SevenSegDisplayingWaterLevel)
+    {
+        if (errors[0]) // we have an error
+        {
+            StateOfSevenSeg = SevenSegDisplayingError;
+            PhaseOfSevenSeg = SevenSegFirstDigit;
+        }
+    }
+
+    switch (StateOfSevenSeg)
+    {
+    case SevenSegDisplayingError:
+        return (previous7SegValue = sevenSegDisplayError());
+        break;
+
+    default: // SevenSegDisplayingWaterLevel
+        return (previous7SegValue = sevenSegDisplayWaterLevel());
+        break;
+    }
+}
+
+void displayInShiftRegister()
+{
+    uint16_t bargraphValue = calculateBarGraph(levelInPercent, 7);
+    uint8_t sevneSegValue = calculate7Seg();
+}
+
+uint16_t measureDistance() // in cm
 {
     statInfo_t xTraStats;
 
@@ -45,17 +178,8 @@ uint16_t MeasureDistance() // in cm
     return distance;
 }
 
-{
-    distance = MeasureDistance();
-
-
-    FullHeight = FULL_WATER + distance;
-    eeprom_write_word(&EE_FullHeight, FullHeight);
-    return 0;
-}
-
 volatile uint8_t AutoRefreshCounter = 0;
-void DisplayInt(uint16_t value, uint8_t isPercent)
+void displayInt(uint16_t value, uint8_t isPercent)
 {
     if (++AutoRefreshCounter == 20)
     {
@@ -86,18 +210,18 @@ void DisplayInt(uint16_t value, uint8_t isPercent)
     SREG = oldSreg;
 }
 
-void DisplayError(uint16_t error_code)
+void displayError(uint16_t error_code)
 {
     while (TRUE)
     {
-        DisplayInt(error_code, FALSE);
+        displayInt(error_code, FALSE);
         _delay_ms(1000);
     }
 }
 
-void FlashValue(uint16_t value)
+void flashValue(uint16_t value)
 {
-    DisplayInt(value, FALSE);
+    displayInt(value, FALSE);
     _delay_ms(1000);
     Clear_Max7219();
     _delay_ms(200);
@@ -108,14 +232,14 @@ void pwmWrite(uint8_t value)
     OCR2B = value;
 }
 
-void initTimer1() 
+void initTimer1()
 {
-	//--------------------------------------------------
-	// Timer1 for the heartbeat update
-	//--------------------------------------------------
-	TCCR1B = (1<<WGM12) | (1<<CS11) | (1<<CS10);//Clear TImer on Compar Match Mode (4), no pin output, TOP=OCR1A, 64 prescaler
-	OCR1A = ((F_CPU / 64) / 100);				//every 10ms
-	TIMSK1 = (1<<OCIE1A);
+    //--------------------------------------------------
+    // Timer1 for the heartbeat update
+    //--------------------------------------------------
+    TCCR1B = (1 << WGM12) | (1 << CS11) | (1 << CS10); // Clear TImer on Compar Match Mode (4), no pin output, TOP=OCR1A, 64 prescaler
+    OCR1A = ((F_CPU / 64) / 100);                      // every 10ms
+    TIMSK1 = (1 << OCIE1A);
 }
 
 void pwmInit()
@@ -134,6 +258,7 @@ void init()
     //--------------------------------------------------
     UCSR0B &= ~_BV(RXEN0); // Disable UART RX
     DDRD = _BV(PIN_UART_TX);
+    DDRD |= _BV(PD4); // PD4 as output for A9 of bargraph
     i2c_init();
     initMillis();
     initTimer1();
@@ -174,8 +299,7 @@ int main(void)
 
     FullHeight = eeprom_read_word(&EE_FullHeight);
 
-    FlashValue(FullHeight); // display full height for 1 sec then clear sceen
-
+    flashValue(FullHeight); // display full height for 1 sec then clear sceen
 
     calibrate();
 
@@ -183,21 +307,21 @@ int main(void)
     {
         heartBeat();
 
-        uint16_t distance = MeasureDistance();
+        uint16_t distance = measureDistance();
         if ((distance | 1) == 8191)
             continue;
 
-        uint16_t percent = ((FullHeight - distance) * 100 / FULL_WATER);
+        levelInPercent = ((FullHeight - distance) * 100 / FULL_WATER);
 
-        if (percent > 100) // update full height
+        if (levelInPercent > 100) // update full height
         {
-            percent = 100;
+            levelInPercent = 100;
             // FullHeight = FULL_WATER + mean_distance;
         }
-        debug_dec(percent);
+        debug_dec(levelInPercent);
         debug_str("% ");
 
-        DisplayInt(percent, TRUE);
+        displayInt(levelInPercent, TRUE);
         _delay_ms(200);
     }
 }
