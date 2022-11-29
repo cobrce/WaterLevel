87fc430857c81d5dadb8d607a9f8e13a62106278
cobrce <cob_rce@hotmail.com>
2022-11-29 03:00:32 +0100

    Updated SevenSeg/Graphbar code (working) and fixed wrong SevenSeg values

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// #define TEST
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/sleep.h>
@@ -37,21 +38,22 @@ volatile uint16_t distance; // for debug
 volatile uint16_t levelInPercent;
 
 // calculate a bargraph representation of a value then shift it to fit the shift register
-uint16_t calculateBarGraph(uint16_t valueInPercent, uint8_t shift)
+uint32_t calculateBarGraph(uint16_t valueInPercent, uint8_t shift)
 {
+
     if (valueInPercent > 100)
         valueInPercent = 100;
-    uint16_t result = 0;
+    uint32_t result = 0;
 
-    valueInPercent = ((valueInPercent / 10) + 1) % 11; // each led in bargraph represents 10% rounded to the next 10%, seiled at 10
+    uint8_t valueInTens = ((valueInPercent / 10) + 1) % 11; // each led in bargraph represents 10% rounded to the next 10%, seiled at 10
 
-    while (valueInPercent--)
+    while (valueInTens--)
         result = (result << 1) | 1;
 
     return result << shift;
 }
 
-// this function calculates the value of 7seg display whether it's an error code or percentage
+// a queue of detected errors to be displayed in in the seven segment
 uint8_t errors[4] = {0};
 
 enum
@@ -67,52 +69,84 @@ enum
     SevenSegThirdDigit
 } PhaseOfSevenSeg = SevenSegFirstDigit;
 
+uint8_t SevenSegFlicker = 0;    // telling "sevenSegDisplayError" and "sevenSegDisplayWaterLevel" that the next char should be blank
+uint8_t SevenSegFlickerEnd = 0; // telling "calculate7Seg" that the previous character was a flicker blank, so updated sooner (100ms instead of 500ms)
+
 uint8_t SevenSegNumbers[] = {
-    0b1110111, // 0
-    0b0110000, // 1
-    0b1101011, // 2
-    0b1111001, // 3
-    0b0111100, // 4
-    0b1011101, // 5
-    0b1011111, // 6
-    0b1110000, // 7
+    0b0111111, // 0
+    0b0000110, // 1
+    0b1011011, // 2
+    0b1001111, // 3
+    0b1100110, // 4
+    0b1101101, // 5
+    0b1111101, // 6
+    0b0000111, // 7
     0b1111111, // 8
-    0b1111101  // 9
+    0b1101111  // 9
 };
 
-const uint8_t SevenSegP = 0b1100111;
-const uint8_t SevenSegF = 0b1000111;
+const uint8_t SevenSegP = 0b1110011;
+const uint8_t SevenSegF = 0b1110001;
+
+uint8_t swapBits(uint8_t input)
+{
+    uint8_t result = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        result <<= 1;
+        result |= (input & 1);
+        input >>= 1;
+    }
+    return result;
+}
+
+uint8_t extractSevenSegDigit(uint32_t value, uint8_t index)
+{
+    for (int i = 0; i < index; i++)
+        value /= 10;
+    value %= 10;
+    return SevenSegNumbers[value]; // convert number into 7 seg representation
+}
 
 volatile uint8_t SevenSegErrorCodeIndex = 1; // like this 2 or 3 chars can be displayed during SevenSegSecondDigit phase and leave third digit for blank char
 uint8_t sevenSegDisplayError()
 {
-    uint8_t result = 0;
+    uint8_t result;
+    result = 0;
     switch (PhaseOfSevenSeg)
     {
     case SevenSegFirstDigit:
         SevenSegErrorCodeIndex = 1;
         PhaseOfSevenSeg = SevenSegSecondDigit;
+        SevenSegFlicker = 1;
         result = SevenSegF;
         break;
 
     case SevenSegSecondDigit:
     {
-        uint8_t value = errors[0];
-        for (int i = 0; i < SevenSegErrorCodeIndex; i++)
-            value /= 10;
-        value %= 10;
-        result = SevenSegNumbers[value]; // convert number into 7 seg representation
-
-        if (SevenSegErrorCodeIndex == 0) // shifting the errors queue
+        if (SevenSegFlicker)
         {
-            for (uint8_t i = 0; i < sizeof(errors); i++)
-                errors[i] = errors[i] + 1;
-            errors[sizeof(errors)-1] = 0;
-            PhaseOfSevenSeg = SevenSegThirdDigit;
+            result = 0;
+            SevenSegFlicker = 0;
+            SevenSegFlickerEnd = 1;
         }
         else
         {
-            SevenSegErrorCodeIndex--;
+            SevenSegFlicker = 1;
+            uint8_t value = errors[0];
+            result = extractSevenSegDigit(value, SevenSegErrorCodeIndex);
+            if (SevenSegErrorCodeIndex == 0) // shifting the errors queue
+            {
+                for (uint8_t i = 0; i < sizeof(errors) - 1; i++)
+                    errors[i] = errors[i + 1];
+                errors[sizeof(errors) - 1] = 0;
+                PhaseOfSevenSeg = SevenSegThirdDigit;
+                SevenSegFlicker = 0;
+            }
+            else
+            {
+                SevenSegErrorCodeIndex--;
+            }
         }
     }
     break;
@@ -120,25 +154,73 @@ uint8_t sevenSegDisplayError()
     case SevenSegThirdDigit:
         StateOfSevenSeg = SevenSegDisplayingWaterLevel; // the last digit of this error is displayed, switching back to water level display, if another error is in queue "Calculate7Seg" will call this again
         PhaseOfSevenSeg = SevenSegFirstDigit;
+        SevenSegFlicker = 0;
         break;
     }
     return result;
 }
 
+uint8_t sevenSegWaterLevelIndex;
+uint8_t levelInPercentBuffered = 0;
 uint8_t sevenSegDisplayWaterLevel()
 {
-    return 0;
+    uint8_t result = 0;
+
+    switch (PhaseOfSevenSeg)
+    {
+    case SevenSegFirstDigit:
+        levelInPercentBuffered = levelInPercent;
+        sevenSegWaterLevelIndex = 2;
+        PhaseOfSevenSeg = SevenSegSecondDigit;
+        SevenSegFlicker = 1;
+        result = SevenSegP;
+        break;
+
+    case SevenSegSecondDigit:
+        if (SevenSegFlicker)
+        {
+            result = 0;
+            SevenSegFlicker = 0;
+            SevenSegFlickerEnd = 1;
+        }
+        else
+        {
+            result = extractSevenSegDigit(levelInPercentBuffered, sevenSegWaterLevelIndex);
+            SevenSegFlicker = 1;
+
+            if (sevenSegWaterLevelIndex == 0)
+            {
+                PhaseOfSevenSeg = SevenSegThirdDigit;
+                SevenSegFlicker = 0;
+            }
+            else
+            {
+                sevenSegWaterLevelIndex--;
+            }
+        }
+
+        break;
+
+    default: // SevenSegThirdDigit
+        PhaseOfSevenSeg = SevenSegFirstDigit;
+        SevenSegFlicker = 0;
+        break;
+    }
+    return result;
 }
 
 volatile uint32_t lastSevenSegUpdate = 0;
 volatile uint8_t previous7SegValue = 0;
+// this function calculates the value of 7seg display whether it's an error code or percentage
 uint8_t calculate7Seg()
 {
+#ifndef TEST
     uint32_t now = millis();
-    if (lastSevenSegUpdate && (now - lastSevenSegUpdate) < 500)
+    if (lastSevenSegUpdate && (now - lastSevenSegUpdate) < (SevenSegFlickerEnd ? 100 : 500))
         return previous7SegValue;
-
     lastSevenSegUpdate = now;
+    SevenSegFlickerEnd = 0;
+#endif
 
     if (StateOfSevenSeg == SevenSegDisplayingWaterLevel)
     {
@@ -146,6 +228,7 @@ uint8_t calculate7Seg()
         {
             StateOfSevenSeg = SevenSegDisplayingError;
             PhaseOfSevenSeg = SevenSegFirstDigit;
+            SevenSegFlicker = 0;
         }
     }
 
@@ -161,15 +244,32 @@ uint8_t calculate7Seg()
     }
 }
 
+#define setA9 PORTD |= _BV(PD4)
+#define clearA9 PORTD &= ~_BV(PD4)
+
 void displayInShiftRegister()
 {
-    uint16_t bargraphValue = calculateBarGraph(levelInPercent, 7);
-    uint8_t sevneSegValue = calculate7Seg();
+
+    uint32_t bargraphValue = calculateBarGraph(levelInPercent, 7);
+    uint8_t sevenSegValue = calculate7Seg();
+
+    uint32_t shiftRegisterValue = bargraphValue | sevenSegValue;
+
+    WriteRegister(swapBits(((uint8_t *)&shiftRegisterValue)[1])); // write the data of the bargraph
+    WriteRegister(((uint8_t *)&shiftRegisterValue)[0]);           // write the data to be displayed by the seven seg
+
+    if ((shiftRegisterValue >> 16) & 1) // complete the data of the bargraph
+        setA9;
+    else
+        clearA9;
+
+    UpdateRegister(); // latch
 }
 
 uint16_t measureDistance() // in cm
 {
     statInfo_t xTraStats;
+    UpdateRegister();
 
     distance = readRangeSingleMillimeters(&xTraStats) / 10;
 
@@ -261,7 +361,9 @@ void init()
     DDRD |= _BV(PD4); // PD4 as output for A9 of bargraph
     i2c_init();
     initMillis();
+#ifndef TEST
     initTimer1();
+#endif
     pwmInit();
     InitShiftRegister();
     sei();
@@ -283,11 +385,55 @@ void heartBeat()
 ISR(TIMER1_COMPA_vect)
 {
     heartBeat();
+    displayInShiftRegister();
+}
+
+void pushError(uint8_t errorValue)
+{
+    for (int i = sizeof(errors) - 1; i> 0;i--)
+        errors[i] = errors[i-1];
+    
+    errors[0] = errorValue;
 }
 
 int main(void)
 {
     init();
+
+#ifdef TEST
+    levelInPercent = 11;
+    int8_t step = 6;
+    int8_t errorCounter = 20;
+    while (1)
+    {
+        if (errorCounter++ == 20)
+        {
+            errors[0] = 12;
+            errors[1] = 24;
+            errorCounter = 0;
+        }
+
+        if (levelInPercent > 90)
+            step = -6;
+        else if (levelInPercent < 11)
+            step = 6;
+        levelInPercent += step;
+        displayInShiftRegister();
+
+        debug_dec(levelInPercentBuffered);
+        debug_putc(' ');
+
+        if (SevenSegFlickerEnd)
+        {
+            SevenSegFlickerEnd = 0;
+            _delay_ms(100);
+        }
+        else
+        {
+            _delay_ms(500);
+        }
+    }
+#endif
 
     initVL53L0X(0);
     // startContinuous(0);
@@ -310,7 +456,10 @@ int main(void)
 
         uint16_t distance = measureDistance();
         if ((distance | 1) == 8191)
+        {
+            pushError(distance - 8100); // either 90 or 91
             continue;
+        }
 
         levelInPercent = ((FullHeight - distance) * 100 / FULL_WATER);
 
